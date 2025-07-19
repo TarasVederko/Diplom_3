f6434f78972f394067f5c5ad03a1507108a53274
Taras Vederko <vederkoat@gmail.com>
2025-07-19 19:16:21 +0300

    Поравил файл url.py

--- url.py
+++ url.py
@@ -1,3 +1,3 @@
 main_site = 'https://stellarburgers.nomoreparties.site/'
-login_url = 'https://stellarburgers.nomoreparties.site/login'
-orders_feed_url = 'https://stellarburgers.nomoreparties.site/feed'
+login_url = f'{main_site}login'
+orders_feed_url = f'{main_site}feed'
